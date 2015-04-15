8b7af7d0b0ffe8f12f362323a53bda8500b37c9f
Sheryl Boughton <sherpbo@gmail.com>
2015-04-15 11:37:53 -0700

    added *, -, /

--- client/index.js
+++ client/index.js
@@ -13,108 +13,144 @@ function getValue(){
   inputString = $('#window').val();
   $('#window').val('');
 
-  if (inputString === '='){
+  if (inputString === '+'){
     // console.log('plus');
     sum();
 
-  } else {
-  var inputNumber = parseInt(inputString);
-  console.log(inputNumber);
-  numbers.push(inputNumber);
+    } else if (inputString === '*'){
+      multiply();
 
-  $('.container > div').text('');
+      } else if (inputString === '-'){
+        subtract();
 
-  numbers.forEach(function(n){
-    var $div = $('<div>');
-    $div.text(n);
-    $('.container').append($div);
-    console.log($('#window').val());
+      } else if (inputString === '/'){
+            divide();
 
-});
-}
-}
-function sum(){
-  debugger;
+      }else {
+        var inputNumber = parseInt(inputString);
+        console.log(inputNumber);
+        numbers.push(inputNumber);
 
-var addedUp = numbers.reduce(function(a, b){
-    return a + b;
-  });
-  console.log(addedUp);
-  var $newDiv = $('<div>');
-  $newDiv.text(addedUp);
-  $('.container').append($newDiv);
-}
+        $('.container > div').text('');
 
+        numbers.forEach(function(n){
+          var $div = $('<div>');
+          $div.text(n);
+          $('.container').append($div);
+          console.log($('#window').val());
 
+      });
+    }
+  }
 
-// $('.container').text(numbers);
+  function divide(){
+    var quotient = numbers.reduce(function(a, b){
+      return a / b;
+      });
+      var $quotientDiv = $('<div>');
+      $quotientDiv.text(quotient);
+      $('.container').append($quotientDiv);
+  }
 
+  function subtract(){
+    var difference = numbers.reduce(function(a, b){
+      return a - b;
+      });
+      var $differenceDiv = $('<div>');
+      $differenceDiv.text(difference);
+      $('.container').append($differenceDiv);
+  }
 
+  function sum(){
 
+    var addedUp = numbers.reduce(function(a, b){
+      return a + b;
+      });
+      console.log(addedUp);
+      var $newDiv = $('<div>');
+      $newDiv.text(addedUp);
+      $('.container').append($newDiv);
+   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   function multiply(){
+     var product = numbers.reduce(function(a, b){
+       return a * b;
+       });
+       var $productDiv = $('<div>');
+       $productDiv.text(product);
+       $('.container').append($productDiv);
+  }
 
 
 
+    // $('.container').text(numbers);
 
 
 
-// console.log(text);
-//
-// var words = text.toLowerCase.match(/\w+/gi);
-//
-// var counts = {};
-//
-// words.forEach(function(word){
-//
-//   counts[word] = counts[word] || 0;
-//   counts[word]++;
-// });
-//
-// console.log('is count:', counts);
 
 
-//
-// function toCamelCase(word){
-// var array = word.split('-');
-// console.log(array);
-//
-// array.forEach(function(n){
-//   console.log(n);
-//   if
-// });
-//
-//
-// }
-//
-// console.log(toCamelCase('coding-house'));
-// console.log(toCamelCase('something-other-than-coding-house'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // console.log(text);
+    //
+    // var words = text.toLowerCase.match(/\w+/gi);
+    //
+    // var counts = {};
+    //
+    // words.forEach(function(word){
+    //
+    //   counts[word] = counts[word] || 0;
+    //   counts[word]++;
+    // });
+    //
+    // console.log('is count:', counts);
+
+
+    //
+    // function toCamelCase(word){
+    // var array = word.split('-');
+    // console.log(array);
+    //
+    // array.forEach(function(n){
+    //   console.log(n);
+    //   if
+    // });
+    //
+    //
+    // }
+    //
+    // console.log(toCamelCase('coding-house'));
+    // console.log(toCamelCase('something-other-than-coding-house'));
